1caee4b12b410282485afc0ac9049d01f53cf36e
Amitraj408 <dynamicamitraj@gmail.com>
2023-06-09 22:58:16 +0530

    commiting in feature branch

--- src/main/java/com/amitraj/onetomanyjavatechie/config/ConfigClass.java
+++ src/main/java/com/amitraj/onetomanyjavatechie/config/ConfigClass.java
@@ -1,4 +1,13 @@
 package com.amitraj.onetomanyjavatechie.config;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ConfigClass {
+
+    @GetMapping("/message")
+    public String getMessage(){
+        return "this is in the feature branch";
+    }
 }
